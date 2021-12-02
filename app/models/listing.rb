49667d30d53cb60e7744612a8bf6f59d1ad8a706
ANGIELOUX <angelajohnson943@gmail.com>
2021-12-03 08:12:10 +1100

    Create model relationships

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  validates :name, presence:true #needs to have name
+  validates :price, presence:true #needs to have price
 end

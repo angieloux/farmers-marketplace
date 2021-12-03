dead47c995a7cfb9c0368edaf89198b5e622b807
ANGIELOUX <angelajohnson943@gmail.com>
2021-12-03 15:30:02 +1100

    Create new/show/index views, form, fix routes
    This commit:
    - Fixes typo in routes & adds resources for listings
    - Adds a form to allow users to create new listings
    - Allows user owner to edit/delete their listing when logged in
    - Builds out show/index view pages
    - Defines new/create/index/update/find_listing/listings_params methods
      in Listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,54 @@
-class ListingController < ApplicationController
-    before_action :authenticate_user!
+class ListingsController < ApplicationController
+    before_action :authenticate_user!, except: %i[index show]
+    before_action :find_listing, only: %i[show edit destroy]
 
     def index
+        @listings = Listing.all
     end
 
     def authenticate_user
+    end
+
+    def show
+    end
+
+    def new
+        # this sends @listing to new.html.erb 
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = current_user.listings.new(listings_params)
+        
+        if @listing.save
+            redirect_to @listing
+        else
+            render :new
+        end
+
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @listing.update(listings_params)
+            redirect_to @listing
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def find_listing
+        @listing = Listing.find(params[:id])
+    end
+
+    def listings_params
+        # because in the create method we already allocate listings to current user, it will already be able to access user_id, so we don't need to specify user_id here:
+        params.require(:listing).permit(:name, :description, :price, :category_id)
     end
     
 end

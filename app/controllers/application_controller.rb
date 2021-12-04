e580abff748d8fb60140f98726d69655eeab4245
ANGIELOUX <angelajohnson943@gmail.com>
2021-12-04 14:19:02 +1100

    Add name field, login/out routes, home welcome msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_paramters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_paramters
+            devise_paramter_sanitizer.permit(:sign_up, keys: [:name])
+            
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+    end
+
   devise_for :users
   resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

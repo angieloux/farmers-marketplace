c66bc8d32065413a0b4b98aeaceed61fc4e4bebc
ANGIELOUX <angelajohnson943@gmail.com>
2021-12-04 12:35:07 +1100

    Add trackable to user model, basic css styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable and :omniauthable
+  devise \
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable,
+    :trackable,
+    :timeoutable, timeout_in: 30.minutes
 
   has_many :listings, dependent: :destroy
 

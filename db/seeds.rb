6761d9e8ec5c62fa22c41f446195fcfb5834f3f9
AJ <angela.johnson943@gmail.com>
2022-11-27 18:14:20 +1100

    Add basic styling w/ bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 meats = %w[Lamb Beef Pork Goat Fish]
 poultries = %w[Chicken Turkey Duck Guineafowl Quail]
 dairies = %w[Butter Eggs Milk Cheese Yoghurt]
+description = Faker::Lorem.paragraph(sentence_count: 3)
 
 if User.count == 0 # if User table is empty 
     User.create! name: "admin", email: "admin@admin.com", password: "password"
@@ -42,15 +43,12 @@ if Category.count == 0 # if Category table is empty
     end
 end
 
-photos = Rails.root.join('/app/assets/images/food/', '*.{jpg,png,gif}')
-
-
 if Listing.count == 0 # if Listing table is empty
     # Create fruits
     5.times do
         id = 1
-        listing = Listing.create! name: Faker::Food.fruits, price: Faker::Number.between(from: 1000, to: 10000), category_id: 2, user_id: id, description: Faker::Lorem.paragraph(sentence_count: 3), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
-        listing.photo.attach(io: '/app/assets/images/food/vegetables.jpeg', filename: 'vegetables.jpeg')
+        listing = Listing.create! name: Faker::Food.fruits, price: Faker::Number.between(from: 1000, to: 10000), category_id: 2, user_id: id, description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/fruit.jpeg'), filename: 'fruit.jpeg')
         listing.save!
 
         rand(1..5).times do
@@ -63,7 +61,9 @@ if Listing.count == 0 # if Listing table is empty
         # Create vegetables 
     5.times do
         id = 1
-        listing = Listing.create! name: Faker::Food.vegetables, price: Faker::Number.between(from: 1000, to: 10000), category_id: 1, user_id: Faker::Number.between(from: 1, to: 11), description: Faker::Lorem.paragraph(sentence_count: 2), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing = Listing.create! name: Faker::Food.vegetables, price: Faker::Number.between(from: 1000, to: 10000), category_id: 1, user_id: Faker::Number.between(from: 1, to: 11), description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/vegetables.jpeg'), filename: 'vegetables.jpeg')
+        listing.save!
 
         rand(1..5).times do
             listing.features << Feature.all.sample
@@ -76,7 +76,9 @@ if Listing.count == 0 # if Listing table is empty
     # Create confectionery
     5.times do
         id = 1
-        listing = Listing.create! name: Faker::Dessert.variety, price: Faker::Number.between(from: 1000, to: 10000), category_id: 3, user_id: Faker::Number.between(from: 1, to: 11), description: Faker::Lorem.paragraph(sentence_count: 2), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing = Listing.create! name: Faker::Dessert.variety, price: Faker::Number.between(from: 1000, to: 10000), category_id: 3, user_id: Faker::Number.between(from: 1, to: 11), description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/confectionery.jpeg'), filename: 'confectionery.jpeg')
+        listing.save!
 
         rand(1..5).times do
             listing.features << Feature.all.sample
@@ -89,7 +91,11 @@ if Listing.count == 0 # if Listing table is empty
     # Create meat 
     meats.each do |meat|
         id = 1
-        listing = Listing.create! name: meat, price: Faker::Number.between(from: 1000, to: 10000), category_id: 6, user_id: Faker::Number.between(from: 1, to: 11), description: Faker::Lorem.paragraph(sentence_count: 2), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing = Listing.create! name: meat, price: Faker::Number.between(from: 1000, to: 10000), category_id: 6, user_id: Faker::Number.between(from: 1, to: 11), description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/meat.webp'), filename: 'meat.webp')
+        listing.save!
+
+
 
         rand(1..5).times do
             listing.features << Feature.all.sample
@@ -103,7 +109,9 @@ if Listing.count == 0 # if Listing table is empty
     # Create poultry
     poultries.each do |poultry|
         id = 1
-        listing = Listing.create! name: poultry, price: Faker::Number.between(from: 1000, to: 10000), category_id: 5, user_id: Faker::Number.between(from: 1, to: 11), description: Faker::Lorem.paragraph(sentence_count: 2), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing = Listing.create! name: poultry, price: Faker::Number.between(from: 1000, to: 10000), category_id: 5, user_id: Faker::Number.between(from: 1, to: 11), description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/poultry.jpeg'), filename: 'poultry.jpeg')
+        listing.save!
 
         rand(1..5).times do
             listing.features << Feature.all.sample
@@ -116,7 +124,9 @@ if Listing.count == 0 # if Listing table is empty
     # Create dairy
     dairies.each do |dairy|
         id = 1
-        listing = Listing.create! name: dairy, price: Faker::Number.between(from: 1000, to: 10000), category_id: 4, user_id: Faker::Number.between(from: 1, to: 11), description: Faker::Lorem.paragraph(sentence_count: 2), username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing = Listing.create! name: dairy, price: Faker::Number.between(from: 1000, to: 10000), category_id: 4, user_id: Faker::Number.between(from: 1, to: 11), description: description, username: User.find(id).name, date_created: (DateTime.now - (rand * 21))
+        listing.image.attach(io: File.open('app/assets/images/food-categories/dairy.jpeg'), filename: 'dairy.jpeg')
+        listing.save!
 
         rand(1..5).times do
             listing.features << Feature.all.sample
